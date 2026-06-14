4d575af4a1fd49276db613c88414873ef9d877a1
dantte <danttepoli3000@gmail.com>
2026-06-13 23:46:14 -0400

    fix sqlite deploy

--- backend/server.js
+++ backend/server.js
@@ -248,5 +248,5 @@ app.get("/estadisticas", verificarToken, (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor corriendo en puerto " + PORT);
+    console.log("Servidor iniciado en puerto " + PORT);
 });
